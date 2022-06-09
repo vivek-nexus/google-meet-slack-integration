42b068f3c6d2f9c04df4939f04551cd82e5cc947
Vivek <audit536@gmail.com>
2022-06-10 00:03:53 +0530

    feat :rocket: Moved to network request intercepting instead of front end

--- background.js
+++ background.js
@@ -1,21 +1,35 @@
+var isStatusSet = false;
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log("Extension loaded");
 
-  if(request.message == "Content Loaded"){
+  if (request.message == "Content Loaded. Watch this tab!") {
     queryTabsInWindow();
-    sendResponse("All set!");
+    sendResponse("All set! Watching this tab.");
   }
 
-  if(request.message == "Set Slack status"){
+  if (request.message == "Set Slack status") {
     setSlackStatus();
     sendResponse("Slack status set!");
   }
-  
-  if(request.message == "Clear Slack status"){
+
+  if (request.message == "Clear Slack status") {
     clearSlackStatus();
     sendResponse("Slack status cleared!");
   }
 });
+
+
+chrome.webRequest.onCompleted.addListener(function () {
+  if (isStatusSet) {
+    console.log("Successfully intercepted network request. Clearing slack status")
+    clearSlackStatus();
+  }
+  else {
+    console.log("Successfully intercepted network request. Setting slack status")
+    setSlackStatus();
+  }
+}, { urls: ["https://meet.google.com/$rpc/google.rtc.meetings.v1.MeetingDeviceService/UpdateMeetingDevice"] })
 
 function queryTabsInWindow() {
   chrome.tabs.query({ url: "https://meet.google.com/*" }, function (tabs) {
@@ -43,16 +57,16 @@ function setSlackStatus() {
   chrome.storage.sync.get(["emojiText", "statusText"], function (result) {
     if (result.emojiText) {
       // https://stackoverflow.com/questions/18862256/how-to-detect-emoji-using-javascript
-      if(/\p{Emoji}/u.test(result.emojiText)){
+      if (/\p{Emoji}/u.test(result.emojiText)) {
         emoji = result.emojiText;
         console.log('One char emoji')
       }
-      else if(/^\:.*\:$/.test(result.emojiText)){
+      else if (/^\:.*\:$/.test(result.emojiText)) {
         emoji = result.emojiText;
         console.log('Custom emoji with both colons')
       }
-      else{
-        emoji = ":"+result.emojiText+":";
+      else {
+        emoji = ":" + result.emojiText + ":";
         console.log('Custom emoji without both colons')
       }
     }
@@ -67,8 +81,9 @@ function setSlackStatus() {
         status_expiration: 0,
       },
     });
-  
+
     makeSlackAPICall(raw, "set");
+    isStatusSet = true;
   });
 }
 
@@ -82,13 +97,14 @@ function clearSlackStatus() {
   });
 
   makeSlackAPICall(raw, "clear");
+  isStatusSet = false;
 }
 
 
 function makeSlackAPICall(raw, type) {
   var key;
   chrome.storage.sync.get(["meetSlackKey"], function (result) {
-    
+
     if (result.meetSlackKey) {
       key = result.meetSlackKey;
 
@@ -108,7 +124,7 @@ function makeSlackAPICall(raw, type) {
 
       fetch("https://slack.com/api/users.profile.set", requestOptions)
         .then((response) => response.text())
-        .then((result) => console.log(result))
+        .then((result) => console.log("Slack status altered"))
         .catch((error) => console.log("error", error));
     }
   });
--- content.js
+++ content.js
@@ -6,10 +6,14 @@ checkExtensionStatus().then((extensionStatus) => {
       let buttons = document.querySelectorAll(".oTVIqe");
       //buttons[0].click(); //turns off microhphone, comment to disable
       buttons[2].click(); //turns off camera, comment to disable
-      contains("div", "Join now")[15].style.display = "none";
 
-      chrome.runtime.sendMessage({ message: "Content Loaded" }, function (response) {
+      chrome.runtime.sendMessage({ message: "Content Loaded. Watch this tab!" }, function (response) {
         console.log(response);
+      });
+
+      chrome.runtime.sendMessage({ message: "Clear Slack status" }, function (response) {
+        console.log(response);
+        console.log("Purging slack status")
       });
 
       chrome.storage.sync.get(["meetSlackKey"], function (result) {
@@ -29,7 +33,6 @@ checkExtensionStatus().then((extensionStatus) => {
           obj.innerText =
             "GMeet-Slack is running. Use Ctrl + V to join meeting, Ctrl + Q to exit meeting.";
           block.prepend(obj);
-          setTimeout(checkClass, 3000);
         }
       });
     });
@@ -55,62 +58,15 @@ checkExtensionStatus().then((extensionStatus) => {
 
 
 
-var meetingType = 'internal';
-
-function checkClass() {
-  if (document.getElementsByClassName("XCoPyb").length) {
-    var main = document.getElementsByClassName("XCoPyb")[0];
-
-    if (contains("div", "Ask to join")[15])
-      meetingType = 'external';
-
-    console.log(meetingType);
-
-    main.prepend(createJoinNowButton(meetingType));
-
-    var joinKey = document.getElementById("meet-slack-join");
-
-    if (joinKey) {
-      joinKey.addEventListener("click", function () {
-        setJoinKey(meetingType);
-      });
-    }
-  } else {
-    console.log("Class does not exist");
-  }
-}
-
-function createJoinNowButton(meetingType) {
-  console.log("createJoinNowButton");
-  var button = document.createElement("div");
-  var span1 = document.createElement("span");
-  var span2 = document.createElement("span");
-
-  // Add attribites to button
-  button.className = "VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qfvgSe jEvJdc QJgqC";
-  button.setAttribute("role", "button");
-  button.setAttribute("tabindex", "0");
-  button.id = "meet-slack-join";
-
-  // Add attribites to span1
-  span1.className = "l4V7wb Fxmcue";
-  span2.className = "NPEfkd RveJvd snByac";
-
-  if (meetingType == 'external')
-    span2.innerText = "Ask to Join";
-  else
-    span2.innerText = "Join Now";
-  span1.append(span2);
-  button.append(span1);
-
-  return button;
-}
-
 document.addEventListener("keydown", function (event) {
   if (event.ctrlKey && event.key === "v") {
     checkExtensionStatus().then(extensionStatus => {
-      if (extensionStatus == 200)
-        setJoinKey(meetingType);
+      if (extensionStatus == 200) {
+        if (contains("div", "Ask to join")[15])
+          contains("div", "Ask to join")[15].firstChild.click();
+        else
+          contains("div", "Join now")[15].firstChild.click();
+      }
     })
   }
 });
@@ -120,38 +76,10 @@ document.addEventListener("keydown", function (event) {
     checkExtensionStatus().then(extensionStatus => {
       if (extensionStatus == 200)
         contains("i", "call_end")[0].parentElement.click();
-      // clearSlackStatus
-      chrome.runtime.sendMessage({ message: "Clear Slack status" }, function (response) {
-        console.log(response);
-      });
     })
   }
 });
 
-function setJoinKey(meetingType) {
-  console.log("Join key pressed");
-  if (meetingType == 'external')
-    contains("div", "Ask to join")[15].firstChild.click();
-  else
-    contains("div", "Join now")[15].firstChild.click();
-
-  // https://stackoverflow.com/a/53269990
-  // Waiting until the host lets you in
-  checkElement('.MQKmmc, .SudKRc, .Q4etDd, .wYNW7d').then((selector) => {
-    // setSlackStatus
-    chrome.runtime.sendMessage({ message: "Set Slack status" }, function (response) {
-      console.log(response);
-    });
-    setTimeout(function () {
-      contains("i", "call_end")[0].parentElement.onclick = function () {
-        // clearSlackStatus
-        chrome.runtime.sendMessage({ message: "Clear Slack status" }, function (response) {
-          console.log(response);
-        });
-      };
-    }, 300);
-  });
-}
 
 function contains(selector, text) {
   var elements = document.querySelectorAll(selector);
@@ -160,17 +88,11 @@ function contains(selector, text) {
   });
 }
 
-// https://stackoverflow.com/a/53269990
-const checkElement = async selector => {
-  while (document.querySelector(selector) === null) {
-    await new Promise(resolve => requestAnimationFrame(resolve))
-  }
-  return document.querySelector(selector);
-};
 
 async function checkExtensionStatus() {
   let status = 400;
 
+  // https://stackoverflow.com/a/42518434
   await fetch("https://yakshag.github.io/gmeet-slack-integration-status/", { cache: "no-store" })
     .then(response => response.text())
     .then(result => {
